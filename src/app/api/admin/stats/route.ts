f8aa7860fc424a6c0efe6a1f1e8b7ff928105064
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2023-09-19 02:07:00 +0530

    Update route.ts

--- src/app/api/admin/stats/route.ts
+++ src/app/api/admin/stats/route.ts
@@ -2,22 +2,34 @@ import {connect} from "@/dbConfig/dbConfig";
 import WaitlistUser from "@/models/waitlistUserModel";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
+import { getServerSession } from "next-auth";
+import { options } from "../../auth/[...nextauth]/options";
 
 connect()
 
 
 export async function GET(request: NextRequest){
 
-    try{
+    try{  
 
-        const userCount = await User.countDocuments();
-        const waitlistUserCount = await WaitlistUser.countDocuments();
+        const session = await getServerSession(options)
+
+        if(session?.user?.role ==="admin"){
+            const userCount = await User.countDocuments();
+            const waitlistUserCount = await WaitlistUser.countDocuments();
+
+            return NextResponse.json({
+                success: true,
+                userCount,
+                waitlistUserCount
+            })      
+
+        }
 
         return NextResponse.json({
-            success: true,
-            userCount,
-            waitlistUserCount
+            success: false,
         })      
+        
   
     }
     catch (error: any) {
